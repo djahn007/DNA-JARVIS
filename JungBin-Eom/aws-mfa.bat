72145155029a993c556487de3bbf08f13214968f
JungBin-Eom <ejb0329@gmail.com>
2022-08-20 00:06:59 +0900

    remove unused file

--- JungBin-Eom/aws-mfa.bat
+++
@@ -1,7 +0,0 @@
-@echo off
- 
-setlocal ENABLEDELAYEDEXPANSION
-
-set otp=%1
-
-aws sts get-session-token --profile=ricky --serial-number="arn:aws:iam::042298199494:mfa/ricky" --token-code="%otp%" | sts2credentials --profile=ricky-mfa
--- JungBin-Eom/eks/tfvars/default.tfvars
+++
@@ -1,4 +0,0 @@
-account_id = "042298199494"
-profile = "ricky-mfa"
-aws_region = "ap-northeast-2"
-instance_types = ["t3.medium", "t3.small"]
