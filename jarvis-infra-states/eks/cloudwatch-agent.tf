4f4bcaa4c706d2c84aafc33dc761e2ce9f9dfbac
JungBin-Eom <ejb0329@gmail.com>
2022-08-20 23:19:52 +0900

    eks: add grafana cloudwatch role

--- jarvis-infra-states/eks/cloudwatch-agent.tf
+++ jarvis-infra-states/eks/cloudwatch-agent.tf
@@ -8,3 +8,14 @@ module "iam_assumable_role_cloudwatch_agent_role" {
     "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
   ]
 }
+
+module "iam_assumable_role_cloudwatch_grafana_role" {
+  source       = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version      = "4.2.0"
+  create_role  = true
+  role_name    = "${local.cluster_name}-cloudwatch-grafana"
+  provider_url = replace(module.eks.cluster_oidc_issuer_url, "https://", "")
+  role_policy_arns = [
+    "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
+  ]
+}
